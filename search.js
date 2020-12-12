d2e3bfd58a344d6e06fe3bf45d590d00d1a27af4
Sitanshu <69389824+Sitanshu13@users.noreply.github.com>
2020-12-12 18:37:19 +0530

    release 1.2

--- search.js
+++ search.js
@@ -16,47 +16,45 @@ sbtn.addEventListener('click', function(){
     let sn = parseInt(l);
     console.log(sn);
     if(sn == '1')
-    {
-        window.open('https://www.google.com/search?client=firefox-b-d&q=' + s[0]);
-        reload = true;
-        iput = ' '
-    }
-    else if(sn == '2')
-    {
-        window.open('https://www.google.com/search?client=firefox-b-d&q=' + s[0] +'+'+s[1]);
-    }
-    else if(sn == 3)
-    {
-        window.open('https://www.google.com/search?client=firefox-b-d&q=' + s[0] +'+'+s[1]+'+'+s[2]);
-    }
-    else if(sn == 4)
-    {
-        window.open('https://www.google.com/search?client=firefox-b-d&q=' + s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]);
-    }
-    else if(sn == 5)
-    {
-        window.open('https://www.google.com/search?client=firefox-b-d&q=' + s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]);
-    }
-    else if(sn == 6)
-    {
-        window.open('https://www.google.com/search?client=firefox-b-d&q=' + s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]+'+'+s[5]);
-    }
-    else if(sn == 7)
-    {
-        window.open('https://www.google.com/search?client=firefox-b-d&q=' + s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]+'+'+s[5]+'+'+s[6]);
-    }
-    else if(sn == 8)
-    {
-        window.open('https://www.google.com/search?client=firefox-b-d&q=' + s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]+'+'+s[5]+'+'+s[6]+'+'+s[7]);
-    }
-    else if(sn == 9)
-    {
-        window.open('https://www.google.com/search?client=firefox-b-d&q=' + s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]+'+'+s[5]+'+'+s[6]+'+'+s[7]+'+'+s[8]);
-    }
-    else if(sn == 10)
-    {
-        window.open('https://www.google.com/search?client=firefox-b-d&q=' + s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]+'+'+s[5]+'+'+s[6]+'+'+s[7]+'+'+s[8]+'+'+s[9]);
-    }
+        {
+            window.open('https://www.google.com/search?sxsrf=ALeKk00_bO9jn7aZctAMQc5A0PyKtjQN-Q%3A1607774993714&source=hp&ei=EbPUX6ekKfSY4-EP3Iar0Ak&q='+ s[0] +'&oq=&gs_lcp=CgZwc3ktYWIQARgAMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnUABYAGDkDmgBcAB4AIABAIgBAJIBAJgBAKoBB2d3cy13aXqwAQo&sclient=psy-ab');
+        }
+        else if(sn == '2')
+        {
+            window.open('https://www.google.com/search?sxsrf=ALeKk00_bO9jn7aZctAMQc5A0PyKtjQN-Q%3A1607774993714&source=hp&ei=EbPUX6ekKfSY4-EP3Iar0Ak&q='+ s[0] +'+'+s[1]+'&oq=&gs_lcp=CgZwc3ktYWIQARgAMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnUABYAGDkDmgBcAB4AIABAIgBAJIBAJgBAKoBB2d3cy13aXqwAQo&sclient=psy-ab');
+        }
+        else if(sn == 3)
+        {
+            window.open('https://www.google.com/search?sxsrf=ALeKk00_bO9jn7aZctAMQc5A0PyKtjQN-Q%3A1607774993714&source=hp&ei=EbPUX6ekKfSY4-EP3Iar0Ak&q='+ s[0] +'+'+s[1]+'+'+s[2]+'&oq=&gs_lcp=CgZwc3ktYWIQARgAMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnUABYAGDkDmgBcAB4AIABAIgBAJIBAJgBAKoBB2d3cy13aXqwAQo&sclient=psy-ab');
+        }
+        else if(sn == 4)
+        {
+            window.open('https://www.google.com/search?sxsrf=ALeKk00_bO9jn7aZctAMQc5A0PyKtjQN-Q%3A1607774993714&source=hp&ei=EbPUX6ekKfSY4-EP3Iar0Ak&q='+ s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'&oq=&gs_lcp=CgZwc3ktYWIQARgAMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnUABYAGDkDmgBcAB4AIABAIgBAJIBAJgBAKoBB2d3cy13aXqwAQo&sclient=psy-ab');
+        }
+        else if(sn == 5)
+        {
+            window.open('https://www.google.com/search?sxsrf=ALeKk00_bO9jn7aZctAMQc5A0PyKtjQN-Q%3A1607774993714&source=hp&ei=EbPUX6ekKfSY4-EP3Iar0Ak&q='+ s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]+'&oq=&gs_lcp=CgZwc3ktYWIQARgAMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnUABYAGDkDmgBcAB4AIABAIgBAJIBAJgBAKoBB2d3cy13aXqwAQo&sclient=psy-ab');
+        }
+        else if(sn == 6)
+        {
+            window.open('https://www.google.com/search?sxsrf=ALeKk00_bO9jn7aZctAMQc5A0PyKtjQN-Q%3A1607774993714&source=hp&ei=EbPUX6ekKfSY4-EP3Iar0Ak&q='+ s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]+'+'+s[5]+'&oq=&gs_lcp=CgZwc3ktYWIQARgAMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnUABYAGDkDmgBcAB4AIABAIgBAJIBAJgBAKoBB2d3cy13aXqwAQo&sclient=psy-ab');
+        }
+        else if(sn == 7)
+        {
+            window.open('https://www.google.com/search?sxsrf=ALeKk00_bO9jn7aZctAMQc5A0PyKtjQN-Q%3A1607774993714&source=hp&ei=EbPUX6ekKfSY4-EP3Iar0Ak&q='+ s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]+'+'+s[5]+'+'+s[6]+'&oq=&gs_lcp=CgZwc3ktYWIQARgAMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnUABYAGDkDmgBcAB4AIABAIgBAJIBAJgBAKoBB2d3cy13aXqwAQo&sclient=psy-ab');
+        }
+        else if(sn == 8)
+        {
+            window.open('https://www.google.com/search?sxsrf=ALeKk00_bO9jn7aZctAMQc5A0PyKtjQN-Q%3A1607774993714&source=hp&ei=EbPUX6ekKfSY4-EP3Iar0Ak&q='+ s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]+'+'+s[5]+'+'+s[6]+'+'+s[7]+'&oq=&gs_lcp=CgZwc3ktYWIQARgAMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnUABYAGDkDmgBcAB4AIABAIgBAJIBAJgBAKoBB2d3cy13aXqwAQo&sclient=psy-ab');
+        }
+        else if(sn == 9)
+        {
+            window.open('https://www.google.com/search?sxsrf=ALeKk00_bO9jn7aZctAMQc5A0PyKtjQN-Q%3A1607774993714&source=hp&ei=EbPUX6ekKfSY4-EP3Iar0Ak&q='+ s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]+'+'+s[5]+'+'+s[6]+'+'+s[7]+'+'+s[8]+'&oq=&gs_lcp=CgZwc3ktYWIQARgAMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnUABYAGDkDmgBcAB4AIABAIgBAJIBAJgBAKoBB2d3cy13aXqwAQo&sclient=psy-ab');
+        }
+        else if(sn == 10)
+        {
+            window.open('https://www.google.com/search?sxsrf=ALeKk00_bO9jn7aZctAMQc5A0PyKtjQN-Q%3A1607774993714&source=hp&ei=EbPUX6ekKfSY4-EP3Iar0Ak&q='+ s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]+'+'+s[5]+'+'+s[6]+'+'+s[7]+'+'+s[8]+'+'+s[9]+'&oq=&gs_lcp=CgZwc3ktYWIQARgAMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnUABYAGDkDmgBcAB4AIABAIgBAJIBAJgBAKoBB2d3cy13aXqwAQo&sclient=psy-ab');
+        }
 });
 
 iput.addEventListener('keyup', (e) => {
@@ -70,43 +68,43 @@ iput.addEventListener('keyup', (e) => {
         console.log(sn);
         if(sn == '1')
         {
-            window.open('https://www.google.com/search?client=firefox-b-d&q=' + s[0]);
+            window.open('https://www.google.com/search?sxsrf=ALeKk00_bO9jn7aZctAMQc5A0PyKtjQN-Q%3A1607774993714&source=hp&ei=EbPUX6ekKfSY4-EP3Iar0Ak&q='+ s[0] +'&oq=&gs_lcp=CgZwc3ktYWIQARgAMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnUABYAGDkDmgBcAB4AIABAIgBAJIBAJgBAKoBB2d3cy13aXqwAQo&sclient=psy-ab');
         }
         else if(sn == '2')
         {
-            window.open('https://www.google.com/search?client=firefox-b-d&q=' + s[0] +'+'+s[1]);
+            window.open('https://www.google.com/search?sxsrf=ALeKk00_bO9jn7aZctAMQc5A0PyKtjQN-Q%3A1607774993714&source=hp&ei=EbPUX6ekKfSY4-EP3Iar0Ak&q='+ s[0] +'+'+s[1]+'&oq=&gs_lcp=CgZwc3ktYWIQARgAMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnUABYAGDkDmgBcAB4AIABAIgBAJIBAJgBAKoBB2d3cy13aXqwAQo&sclient=psy-ab');
         }
         else if(sn == 3)
         {
-            window.open('https://www.google.com/search?client=firefox-b-d&q=' + s[0] +'+'+s[1]+'+'+s[2]);
+            window.open('https://www.google.com/search?sxsrf=ALeKk00_bO9jn7aZctAMQc5A0PyKtjQN-Q%3A1607774993714&source=hp&ei=EbPUX6ekKfSY4-EP3Iar0Ak&q='+ s[0] +'+'+s[1]+'+'+s[2]+'&oq=&gs_lcp=CgZwc3ktYWIQARgAMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnUABYAGDkDmgBcAB4AIABAIgBAJIBAJgBAKoBB2d3cy13aXqwAQo&sclient=psy-ab');
         }
         else if(sn == 4)
         {
-            window.open('https://www.google.com/search?client=firefox-b-d&q=' + s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]);
+            window.open('https://www.google.com/search?sxsrf=ALeKk00_bO9jn7aZctAMQc5A0PyKtjQN-Q%3A1607774993714&source=hp&ei=EbPUX6ekKfSY4-EP3Iar0Ak&q='+ s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'&oq=&gs_lcp=CgZwc3ktYWIQARgAMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnUABYAGDkDmgBcAB4AIABAIgBAJIBAJgBAKoBB2d3cy13aXqwAQo&sclient=psy-ab');
         }
         else if(sn == 5)
         {
-            window.open('https://www.google.com/search?client=firefox-b-d&q=' + s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]);
+            window.open('https://www.google.com/search?sxsrf=ALeKk00_bO9jn7aZctAMQc5A0PyKtjQN-Q%3A1607774993714&source=hp&ei=EbPUX6ekKfSY4-EP3Iar0Ak&q='+ s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]+'&oq=&gs_lcp=CgZwc3ktYWIQARgAMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnUABYAGDkDmgBcAB4AIABAIgBAJIBAJgBAKoBB2d3cy13aXqwAQo&sclient=psy-ab');
         }
         else if(sn == 6)
         {
-            window.open('https://www.google.com/search?client=firefox-b-d&q=' + s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]+'+'+s[5]);
+            window.open('https://www.google.com/search?sxsrf=ALeKk00_bO9jn7aZctAMQc5A0PyKtjQN-Q%3A1607774993714&source=hp&ei=EbPUX6ekKfSY4-EP3Iar0Ak&q='+ s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]+'+'+s[5]+'&oq=&gs_lcp=CgZwc3ktYWIQARgAMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnUABYAGDkDmgBcAB4AIABAIgBAJIBAJgBAKoBB2d3cy13aXqwAQo&sclient=psy-ab');
         }
         else if(sn == 7)
         {
-            window.open('https://www.google.com/search?client=firefox-b-d&q=' + s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]+'+'+s[5]+'+'+s[6]);
+            window.open('https://www.google.com/search?sxsrf=ALeKk00_bO9jn7aZctAMQc5A0PyKtjQN-Q%3A1607774993714&source=hp&ei=EbPUX6ekKfSY4-EP3Iar0Ak&q='+ s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]+'+'+s[5]+'+'+s[6]+'&oq=&gs_lcp=CgZwc3ktYWIQARgAMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnUABYAGDkDmgBcAB4AIABAIgBAJIBAJgBAKoBB2d3cy13aXqwAQo&sclient=psy-ab');
         }
         else if(sn == 8)
         {
-            window.open('https://www.google.com/search?client=firefox-b-d&q=' + s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]+'+'+s[5]+'+'+s[6]+'+'+s[7]);
+            window.open('https://www.google.com/search?sxsrf=ALeKk00_bO9jn7aZctAMQc5A0PyKtjQN-Q%3A1607774993714&source=hp&ei=EbPUX6ekKfSY4-EP3Iar0Ak&q='+ s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]+'+'+s[5]+'+'+s[6]+'+'+s[7]+'&oq=&gs_lcp=CgZwc3ktYWIQARgAMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnUABYAGDkDmgBcAB4AIABAIgBAJIBAJgBAKoBB2d3cy13aXqwAQo&sclient=psy-ab');
         }
         else if(sn == 9)
         {
-            window.open('https://www.google.com/search?client=firefox-b-d&q=' + s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]+'+'+s[5]+'+'+s[6]+'+'+s[7]+'+'+s[8]);
+            window.open('https://www.google.com/search?sxsrf=ALeKk00_bO9jn7aZctAMQc5A0PyKtjQN-Q%3A1607774993714&source=hp&ei=EbPUX6ekKfSY4-EP3Iar0Ak&q='+ s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]+'+'+s[5]+'+'+s[6]+'+'+s[7]+'+'+s[8]+'&oq=&gs_lcp=CgZwc3ktYWIQARgAMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnUABYAGDkDmgBcAB4AIABAIgBAJIBAJgBAKoBB2d3cy13aXqwAQo&sclient=psy-ab');
         }
         else if(sn == 10)
         {
-            window.open('https://www.google.com/search?client=firefox-b-d&q=' + s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]+'+'+s[5]+'+'+s[6]+'+'+s[7]+'+'+s[8]+'+'+s[9]);
+            window.open('https://www.google.com/search?sxsrf=ALeKk00_bO9jn7aZctAMQc5A0PyKtjQN-Q%3A1607774993714&source=hp&ei=EbPUX6ekKfSY4-EP3Iar0Ak&q='+ s[0] +'+'+s[1]+'+'+s[2]+'+'+s[3]+'+'+s[4]+'+'+s[5]+'+'+s[6]+'+'+s[7]+'+'+s[8]+'+'+s[9]+'&oq=&gs_lcp=CgZwc3ktYWIQARgAMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnMgcIIxDqAhAnUABYAGDkDmgBcAB4AIABAIgBAJIBAJgBAKoBB2d3cy13aXqwAQo&sclient=psy-ab');
         }
     }
 });
